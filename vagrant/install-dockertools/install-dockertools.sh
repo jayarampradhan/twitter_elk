1b6e80a2eaea8c93aa747b64158bae91e66d1317
jpradhan <jayaram_pradhan@intuit.com>
2016-07-07 19:54:21 +0530

    Creating the repo

--- vagrant/install-dockertools/install-dockertools.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo yum install -y epel-release
-sudo yum install -y python-pip
-sudo curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
